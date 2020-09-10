e65f192b5675c25c8bc9bd09eb2d027b219d1d86
= <=>
2020-09-10 06:33:32 -0700

    added module for user input of variable

--- sample.py
+++ sample.py
@@ -3,7 +3,9 @@ A Python program to solve the
 quadratic function  ax2 + bx + c=0 using
 formula
 '''
-a, b, c = 1, 6 ,9
+a=int(input("enter the value of a "))
+b= int(input("enter the value of b"))
+c=int(input("enter the value of c"))
 
 x1= (-b + ((b*b)-(4*a*c)) ** 0.5)/2 *a
 x2= (-b + ((b*b)-(4*a*c)) ** 0.5)/2 *a
